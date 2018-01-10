b5e170c366fb0ee8962c0d674929738535cf489c
DESKTOP-71QGMBU\Fah <maneewanj57@email.nu.ac.th>
2018-01-10 17:51:06 +0700

    exam1

--- examination/examp1.php
+++ examination/examp1.php
@@ -1,2 +1,28 @@
 <?php
-echo "HELLO NEW INTERN";
+for ($j=1 ; $j<=5 ; $j++)
+{
+    for ($k=1 ; $k<=$j ;$k++)
+    {
+        echo"*";
+        if($j==$k)
+        {
+            echo "<br>";
+        }
+    }
+}
+for ($j=5 ; $j>=1 ; $j--)
+{
+    for ($k=1 ; $k<=$j ; $k++ )
+    {
+        echo"*";
+        if($k==$j)
+        {
+            echo "<br>";
+        }
+    }
+}
+?>
+
+
+
+
