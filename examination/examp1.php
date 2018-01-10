94b6a112cea0940b71b28f98ddc41575e08031b4
PC\User <aodchaopa@gmail.com>
2018-01-10 17:51:20 +0700

    ex1

--- examination/examp1.php
+++ examination/examp1.php
@@ -1,2 +1,23 @@
 <?php
-echo "HELLO NEW INTERN";
+
+ for($i=1;$i<=5;$i++)
+ {
+     for($k=1;$k<=$i;$k++){
+         echo "*";
+         if($i==$k){
+             echo"<br>";
+         }
+     }
+
+ }
+for($i=5;$i>=1;$i--)
+{
+    for($k=1;$k<=$i;$k++){
+        echo "*";
+        if($i==$k){
+            echo"<br>";
+        }
+    }
+
+}
+?>
