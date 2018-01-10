6476c9a04e2de3a100e4e073ac1165ad42339c19
DESKTOP-M9L2PFT\ploy <praphasirit57@email.nu.ac.th>
2018-01-10 22:40:40 +0700

    exam2

--- examination/examp2.php
+++ examination/examp2.php
@@ -1,1 +1,17 @@
+<table>
 <?php
+
+    for($p=1; $p<=6; $p++)
+    {
+        echo "<tr>";
+        for($k=1; $k<=6; $k++)
+        {
+            echo "<td>";
+            $u=$p*$k;
+            echo "$u";
+            echo "</td>";
+        }
+        echo "</tr>";
+    }
+    ?>
+</table>
