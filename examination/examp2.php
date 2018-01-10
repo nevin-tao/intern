b696b13030e60e28d10ffa7bb0bcbbca512c3435
DESKTOP-71QGMBU\Fah <maneewanj57@email.nu.ac.th>
2018-01-10 23:03:29 +0700

    exam2

--- examination/examp2.php
+++ examination/examp2.php
@@ -1,1 +1,17 @@
-<?php
+<table>
+    <?php
+
+    for($j=1; $j<=6; $j++)
+    {
+        echo "<tr>";
+        for($k=1; $k<=6; $k++)
+        {
+            echo "<td>";
+            $i=$j*$k;
+            echo "$i";
+            echo "</td>";
+        }
+        echo "</tr>";
+    }
+    ?>
+</table>
