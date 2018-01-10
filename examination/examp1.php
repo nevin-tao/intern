d484b49fae2bb8e9b53c684e00dc1962d8c7ba91
BB-EASY\Admins <piratefreewin22@gmail.com>
2018-01-10 17:59:24 +0700

    ex1

--- examination/examp1.php
+++ examination/examp1.php
@@ -1,2 +1,21 @@
 <?php
-echo "HELLO NEW INTERN";
+    for($i=1;$i<=5;$i++){
+        for($h=1;$h<=$i;$h++){
+            echo"*";
+            if($i==$h){
+                echo"<br>";
+            }
+
+        }
+    }
+for($i=5;$i>=1;$i--){
+    for($h=1;$h<=$i;$h++){
+        echo"*";
+        if($i==$h){
+            echo"<br>";
+        }
+
+    }
+}
+
+?>
