bf543995fcdd6f8bd8eecb5bd93c3159a1a0d522
Umnuaychai <pondumnuaychai@gmail.com>
2018-01-10 17:52:44 +0700

    examp1 examp2

--- examination/examp1.php
+++ examination/examp1.php
@@ -1,2 +1,17 @@
 <?php
-echo "HELLO NEW INTERN";
+
+    for($i=1; $i<=5; $i++){
+        for($j=1; $j<=$i; $j++){
+            echo "*";
+        }
+        echo "<br/>";
+    }
+
+    for($i=5; $i>=1; $i--){
+        for($j=1; $j<=$i; $j++){
+            echo "*";
+        }
+        echo "<br/>";
+    }
+
+?>
--- examination/examp2.php
+++ examination/examp2.php
@@ -1,1 +1,13 @@
 <?php
+$count ="";
+ for ($i=1; $i<=6; $i++){
+     for($j=1; $j<=6; $j++){
+         if(strlen(($i*$j)) < 2 ) {
+             $count = $count ."&nbsp;" ." &nbsp;  " .($i * $j);
+         }else
+             $count = $count ."&nbsp; " .($i * $j);
+     }
+     echo $count ."<br/>";
+     $count = "";
+ }
+?>
