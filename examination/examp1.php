6ec9a745c76c0b759db259d2cd941dd4dd641a72
BB-EASY\Admins <s57127328119@ssru.ac.th>
2018-01-10 17:54:38 +0700

    examp1

--- examination/examp1.php
+++ examination/examp1.php
@@ -1,2 +1,21 @@
 <?php
-echo "HELLO NEW INTERN";
+for($j=1;$j<=5;$j++){
+    for($k=1;$k<=$j;$k++) {
+            echo "*";
+            if($j==$k){
+            echo "<br>";
+        }
+
+    }
+}
+for($j=5;$j>=1;$j--) {
+    for($k=1;$k<=$j;$k++) {
+        echo "*";
+        if($j==$k){
+            echo "<br>";
+        }
+
+    }
+}
+
+?>
