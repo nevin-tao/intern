62ef890d552bb93172c9e7ac6b600ce8cb9a3d64
DESKTOP-71QGMBU\Fah <maneewanj57@email.nu.ac.th>
2018-01-11 14:24:50 +0700

    exam9

--- examination/examp9.php
+++ examination/examp9.php
@@ -1,1 +1,12 @@
 <?php
+$d1=12;
+$d2=6;
+$d3=$d1+$d2;
+$d4=$d1-$d2;
+$d5=$d1*$d2;
+$d6=$d1/$d2;
+ echo "Add $d3" ;
+ echo "<br> Minus $d4" ;
+ echo "<br> Multiply $d5";
+ echo "<br> Divide $d6";
+ ?>
