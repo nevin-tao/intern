ae26e0ed932a22552240d239ed3098ae9f646e84
DESKTOP-M9L2PFT\ploy <praphasirit57@email.nu.ac.th>
2018-01-11 11:39:25 +0700

    exam5

--- examination/examp5.php
+++ examination/examp5.php
@@ -1,1 +1,15 @@
 <?php
+$ceu = array("Italy"=>"Rome","Luxembourg"=>"Luxembourg","Belgium"=>"Brussel","Denmark"=>"Copenhagen","Finland"=>"Helsinki",
+    "France"=>"Paris","Slovakia"=>"Bratislava","Slovenia"=>"Ljubljana","Germany"=>"Berlin","Greece"=>"Athens","Ireland"=>"Dublin",
+    "Netherlands"=>"Amsterdam","Portugal"=>"Lisbon","Spain"=>"Madrid","Sweden"=>"Stockholm","United Kingdon"=>"London","Cyprus"=>"Nicosia",
+    "Lithuania"=>"Vilnius","Czech Republic"=>"Prague","Estonia"=>"Tallin","Hungary"=>"Budapest","Latvia"=>"Riga","Malta"=>"Valetta",
+    "Austria"=>"Vienna","Poland"=>"Warsaw");
+
+
+
+    foreach ($ceu as $p => $p_value){
+        echo "The capital of $p"."&nbsp"."is $p_value";
+        echo "<br>";
+    }
+echo "<br>";
+?>
