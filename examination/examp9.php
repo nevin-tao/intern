952a587b70d95021ef9b77ffab34c24615577930
DESKTOP-M9L2PFT\ploy <praphasirit57@email.nu.ac.th>
2018-01-11 14:17:40 +0700

    exam9

--- examination/examp9.php
+++ examination/examp9.php
@@ -1,1 +1,15 @@
-<?php
+<?PHP
+
+$num1 = 12;
+$num2 = 6;
+
+echo "Add = ".( $num1 + $num2 );
+echo "<br/>";
+echo "Subtract = ".( $num1 - $num2 );
+echo "<br/>";
+echo "Multiply = ".( $num1 * $num2 );
+echo "<br/>";
+echo "Divide = ".( $num1 / $num2 );
+echo "<br/>";
+
+?>
