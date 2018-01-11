117c69d258b4bf28a63aa2358e75d73689bf2c78
PC\User <aodchaopa@gmail.com>
2018-01-11 12:19:00 +0700

    ex6

--- examination/examp6.php
+++ examination/examp6.php
@@ -1,13 +1,13 @@
 <?php
 $original = array( '1','2','3','4','5' );
-echo 'Original array : '."\n";
+echo "Original array : ";
 echo "<br>";
 foreach ($original as $x)
 {echo "$x ";}
 echo "<br>";
 $inserted = '$';
 array_splice( $original, 3, 0, $inserted );
-echo " \n After inserting '$' the array is : "."\n";
+echo " After inserting '$' the array is : ";
 echo "<br>";
 foreach ($original as $x)
 {echo "$x ";}
