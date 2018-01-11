0d5d279bf95e59a9e661f2f86d9257e0cf40d6b0
PC\User <aodchaopa@gmail.com>
2018-01-11 11:49:42 +0700

    ex5

--- examination/examp5.php
+++ examination/examp5.php
@@ -1,1 +1,15 @@
 <?php
+$ceu = array("Itary"=>"Rome","Luxembourg"=>"Luxembourg","Belgium"=>"Brussels",
+    "Denmark"=>"Copenhagen","Finland"=>"Helsinki","France"=>"Pris","Slovakia"=>"Bratislava",
+    "Slovenia"=>"Ljubljana","Germany"=>"Berlin","Greece"=>"Athens","Ireland"=>"Dublin",
+    "Netherlands"=>"Amsterdam","Portugal"=>"Lisbon","Spain"=>"Madrid","Sweden"=>"Stockholm",
+    "United kingdom"=>"London","Cyprus"=>"Nicosi","Lithuania"=>"Vilnius","Czech Republic"=>"Prague",
+    "Estonia"=>"Tallin","Hungary"=>"Budapest","Latvia"=>"Riga","Malta"=>"Valetta","Austria",
+    "Poland"=>"Warsaw");
+
+foreach($ceu as $x => $x_value){
+    echo "The capital of $x"."&nbsp"." is ".$x_value;
+    echo "<br>";
+}
+
+?>
