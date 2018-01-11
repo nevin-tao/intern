25f698d7795d5dd7cbcf07eabdd6b4e421c2c876
PC\User <aodchaopa@gmail.com>
2018-01-11 15:01:15 +0700

    ex8 ex9 ex10

--- examination/examp10.php
+++ examination/examp10.php
@@ -1,1 +1,5 @@
 <?php
+
+$localIP = getHostByName(getHostName());
+echo "$localIP";
+?>
--- examination/examp9.php
+++ examination/examp9.php
@@ -1,1 +1,26 @@
 <?php
+class MyCalculator {
+    private $_fval, $_sval;
+    public function __construct( $fval, $sval ) {
+        $this->_fval = $fval;
+        $this->_sval = $sval;
+    }
+    public function add() {
+        return $this->_fval + $this->_sval;
+    }
+    public function subtract() {
+        return $this->_fval - $this->_sval;
+    }
+    public function multiply() {
+        return $this->_fval * $this->_sval;
+    }
+    public function divide() {
+        return $this->_fval / $this->_sval;
+    }
+}
+$mycalc = new MyCalculator(12, 6);
+echo $mycalc-> add()."<br>";
+echo $mycalc-> multiply()."<br>";
+echo $mycalc-> subtract()."<br>";
+echo $mycalc-> divide()."<br>";
+?>
