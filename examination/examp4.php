a090cdb0e6af2478786b52fd2cc914f4b7e3fc44
PC\User <aodchaopa@gmail.com>
2018-01-11 10:54:08 +0700

    ex4

--- examination/examp4.php
+++ examination/examp4.php
@@ -1,1 +1,14 @@
 <?php
+$n = 5;
+echo "\n";
+$count = 1;
+for ($i = $n; $i > 0; $i--)
+{
+    for ($j = $i; $j < $n + 1; $j++)
+    {
+        printf("%4s", $count);
+        $count++;
+    }
+    echo "<br>";
+}
+?>
